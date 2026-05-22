98af24d103289a2da041a873150cdbd47fad1333
ShevchenkoO <shevchenko.sasha2005@gmail.com>
2026-05-22 16:33:41 +0200

    added: react router

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
-
+import {Routes, Route} from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 function App() {
 
     return (
         <>
-
+            <Routes>
+                <Route path="/login" element={<LoginPage/>}/>
+                {/*<Route path="/register" element={<RegisterPage/>}/>*/}
+            </Routes>
         </>
     )
 }
